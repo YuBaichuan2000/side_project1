4abed96dc417fab66d2126a4fe076e078d7ec9b1
George Yu <george@think.digital>
2024-01-05 18:17:07 +1030

    add interactive greeting js function, well done

--- hello.js
+++ hello.js
@@ -1,7 +1,21 @@
+// alert interaction
 document.addEventListener('DOMContentLoaded', function(){
-    document.querySelector('form').addEventListener('submit', function(event){
-        let name = document.querySelector('#name').value;
+    document.querySelector('.form1').addEventListener('submit', function(event){
+        let name = document.querySelector('.form1 #name').value;
         alert('hello ' + name);
         event.preventDefault();
     })
 })
+// greeting interaction
+document.addEventListener('DOMContentLoaded', function(){
+    let input = document.querySelector('.form2 input');
+    input.addEventListener('keyup', function(event){
+        let name = document.querySelector('.form2 p');
+        if (input.value){
+            name.innerHTML = `hello, ${input.value}`;
+        }
+        else{
+            name.innerHTML = 'hello, whoever you are';
+        }
+    });
+});
