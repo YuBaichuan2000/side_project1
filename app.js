24ba4b1258f2bec1eee6287ec3baa77e32edf413
George Yu <george@think.digital>
2024-01-08 14:32:18 +1030

    Add background change feature

--- app.js
+++ app.js
@@ -15,3 +15,20 @@ input.addEventListener('keyup', function(event){
         name.innerHTML = 'hello, whoever you are';
     }
 });
+
+let body = document.querySelector('body');
+document.querySelector('#red').addEventListener('click', ()=>{
+    body.style.backgroundColor = 'red';
+});
+document.querySelector('#green').addEventListener('click', ()=>{
+    body.style.backgroundColor = 'green';
+});
+document.querySelector('#blue').addEventListener('click', ()=>{
+    body.style.backgroundColor = 'blue';
+});
+
+function reset(){
+    body.style.backgroundColor = 'white';
+}
+
+window.setInterval(reset, 2000);
